97e4d7e36cc706bdc25e8adeefdaa3ae49b0a435
SuperKojima <hayato.kojima@steamship.co.jp>
2025-01-30 14:04:42 +0900

    :clown_face:とりあえず情報取得できた

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { Hono } from 'hono'
+import { neon } from '@neondatabase/serverless';
+// import { drizzle } from 'drizzle-orm/neon-serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { usersTable } from './schema';
 
 interface Env {
   DATABASE_URL: string
@@ -9,6 +13,12 @@ const app = new Hono<{ Bindings: Env }>()
 app
   .get('/', (c) => {
     return c.text('Hello Hono!')
+  })
+  .get('/users', async (c) => {
+    const sql = neon(c.env.DATABASE_URL)
+    const db = drizzle(sql)
+    const users = await db.select().from(usersTable)
+    return c.json(users)
   })
 
 export default app
