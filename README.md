bd8abc9b233d5ffa3249205ca9828cac62782241
SuperKojima <hayato.kojima@steamship.co.jp>
2025-01-30 15:18:28 +0900

    :ambulance: やっぱり壊れた

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - Neon
 - Cloudflare Workers
 - Cloudflare Hyperdrive
+
+```
+export WRANGLER_HYPERDRIVE_LOCAL_CONNECTION_STRING_HYPERDRIVE="database_url"
+```
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
+import type { NeonDatabase } from 'drizzle-orm/neon-serverless';
 import { Hono } from 'hono'
-import { neon } from '@neondatabase/serverless';
-// import { drizzle } from 'drizzle-orm/neon-serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { Client, neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
+import { drizzle as d } from 'drizzle-orm/neon-http';
 import { usersTable } from './schema';
 
 interface Env {
   DATABASE_URL: string
+  HYPERDRIVE: Hyperdrive
 }
 
 const app = new Hono<{ Bindings: Env }>()
@@ -14,15 +16,18 @@ app
   .get('/', (c) => {
     return c.text('Hello Hono!')
   })
-  .get('/users', async (c) => {
-    const sql = neon(c.env.DATABASE_URL)
-    const db = drizzle(sql)
-    const users = await db.select().from(usersTable)
-    return c.json(users)
-  })
+  // .get('/users', async (c) => {
+  //   const sql = neon(c.env.DATABASE_URL)
+  //   const db = drizzle(sql)
+  //   const users = await db.select().from(usersTable)
+  //   return c.json(users)
+  // })
   .get('/users-hd', async (c) => {
-    const sql = neon(c.env.DATABASE_URL)
-    const db = drizzle(sql)
+    const sql = neon(c.env.HYPERDRIVE.connectionString)
+    console.log({
+      HYPERDRIVE: c.env.HYPERDRIVE,
+    })
+    const db = d(sql)
     const users = await db.select().from(usersTable)
     return c.json(users)
   })
