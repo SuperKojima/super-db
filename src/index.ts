fc1ce431d23bf7f32e8cf34f8fe33a487d50c773
SuperKojima <hayato.kojima@steamship.co.jp>
2025-01-30 16:24:16 +0900

    :art: paste errors

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,6 @@ export default {
     //   }
     // }
 
-
     try {
       const result = await sql`SELECT * from public."users"`;
       return Response.json({ result });
@@ -31,5 +30,24 @@ export default {
         { status: 500 }
       );
     }
+
+    //     ✘ [ERROR] PostgresError: connection is insecure (try using `sslmode=require`)
+    //     at ErrorResponse
+    // (file:///Users/kjm/dev/super-db/node_modules/postgres/cf/src/connection.js:790:26)
+    //     at handle
+    // (file:///Users/kjm/dev/super-db/node_modules/postgres/cf/src/connection.js:475:7)
+    //     at EventEmitter.data
+    // (file:///Users/kjm/dev/super-db/node_modules/postgres/cf/src/connection.js:317:9)
+    //     at EventEmitter.emit (node-internal:events:307:32)
+    //     at read (file:///Users/kjm/dev/super-db/node_modules/postgres/cf/polyfills.js:202:13)
+    //     at cachedError
+    // (file:///Users/kjm/dev/super-db/node_modules/postgres/cf/src/query.js:170:23)
+    //     at new Query
+    // (file:///Users/kjm/dev/super-db/node_modules/postgres/cf/src/query.js:36:24)
+    //     at sql2 (file:///Users/kjm/dev/super-db/node_modules/postgres/cf/src/index.js:113:11)
+    //     at Object.fetch (file:///Users/kjm/dev/super-db/src/index.ts:18:28) {
+    //   severity_local: 'ERROR',
+    //   code: 'XX000'
+    // }
   },
 } satisfies ExportedHandler<Env>;
